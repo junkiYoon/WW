af84395c900254628083306cab0d98122a3e2d9e
junkiYoon <juneki4933@gmail.com>
2022-04-25 14:36:12 +0900

    CREATE: auth endpoint

---
+++ server/src/apps/auth.py
@@ -0,0 +1,44 @@
+from fastapi import APIRouter, status, Depends
+
+from src import Settings, get_settings
+
+from src.core.schemas.auth import Sign_up, Sign_in
+
+from src.core.models import session_scope
+
+from src.utils.auth import is_email, is_nickname, create_user, login
+
+
+router = APIRouter()
+
+
+@router.get("/email", status_code=status.HTTP_200_OK, tags=["AUTH"])
+async def email_validation(email: str, settings: Settings = Depends(get_settings())):
+    with session_scope(settings.MYSQL_DB_URL) as session:
+        response = await is_email(session=session, email=email)
+
+        return response
+
+
+@router.get("/nickname", status_code=status.HTTP_200_OK, tags=["AUTH"])
+async def nickname_validation(nickname: str, settings: Settings = Depends(get_settings())):
+    with session_scope(settings.MYSQL_DB_URL) as session:
+        response = await is_nickname(session=session, nickname=nickname)
+
+        return response
+
+
+@router.post("/signup", status_code=status.HTTP_201_CREATED, tags=["AUTH"])
+def sign_up(body: Sign_up, settings: Settings = Depends(get_settings())):
+    with session_scope(settings.MYSQL_DB_URL) as session:
+        response = create_user(session=session, email=body.email, password=body.password, nickname=body.nickname)
+
+        return response
+
+
+@router.post("/signin", status_code=status.HTTP_200_OK, tags=["AUTH"])
+def sign_in(body: Sign_in, settings: Settings = Depends(get_settings())):
+    with session_scope(settings.MYSQL_DB_URL) as session:
+        response = login(session=session, email=body.email, password=body.password)
+
+        return response
