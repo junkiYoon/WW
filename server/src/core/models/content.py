46ea0569a3e431b0162c8058ff3da2601a453b3f
junkiYoon <juneki4933@gmail.com>
2022-04-26 16:35:50 +0900

    UPDATE: fix typing error

--- server/src/core/models/content.py
+++ server/src/core/models/content.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, INTEGER, VARCHAR, TEXT
 from src.core.models import Base
 
 
-class content_tbl(Base):
+class Content_tbl(Base):
     __tablename__ = "content_tbl"
 
     id = Column(INTEGER, primary_key=True, autoincrement=True)
