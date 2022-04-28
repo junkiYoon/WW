11f1f6657a6655d9f0caa882cc518afdd0d74b5f
junkiYoon <juneki4933@gmail.com>
2022-04-28 13:51:30 +0900

    FIX: typing error

--- server/src/apps/auth.py
+++ server/src/apps/auth.py
@@ -13,7 +13,7 @@ router = APIRouter()
 
 
 @router.get("/email", status_code=status.HTTP_200_OK)
-async def email_validation(email: str, settings: Settings = Depends(get_settings())):
+async def email_validation(email: str, settings: Settings = Depends(get_settings)):
     with session_scope(settings.MYSQL_DB_URL) as session:
         response = await is_email(session=session, email=email)
 
@@ -21,7 +21,7 @@ async def email_validation(email: str, settings: Settings = Depends(get_settings
 
 
 @router.get("/nickname", status_code=status.HTTP_200_OK)
-async def nickname_validation(nickname: str, settings: Settings = Depends(get_settings())):
+async def nickname_validation(nickname: str, settings: Settings = Depends(get_settings)):
     with session_scope(settings.MYSQL_DB_URL) as session:
         response = await is_nickname(session=session, nickname=nickname)
 
@@ -29,7 +29,7 @@ async def nickname_validation(nickname: str, settings: Settings = Depends(get_se
 
 
 @router.post("/signup", status_code=status.HTTP_201_CREATED)
-def sign_up(body: Sign_up, settings: Settings = Depends(get_settings())):
+def sign_up(body: Sign_up, settings: Settings = Depends(get_settings)):
     with session_scope(settings.MYSQL_DB_URL) as session:
         response = create_user(session=session, email=body.email, password=body.password, nickname=body.nickname)
 
@@ -37,7 +37,7 @@ def sign_up(body: Sign_up, settings: Settings = Depends(get_settings())):
 
 
 @router.post("/signin", status_code=status.HTTP_200_OK)
-def sign_in(body: Sign_in, settings: Settings = Depends(get_settings())):
+def sign_in(body: Sign_in, settings: Settings = Depends(get_settings)):
     with session_scope(settings.MYSQL_DB_URL) as session:
         response = login(session=session, email=body.email, password=body.password)
 
